2160d2f454c0b8df0610436bc67f366e0cf21f5f
Evster <8961240@gmail.com>
2021-04-25 01:44:01 +0700

    Added ru-locale for errors

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,14 +2,14 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
+        <div class="alert alert-danger" role="alert">
+            <h6>Возникли следующие проблемы:</h5>
+            <hr>
+            <ol class="">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ol>
         </div>
-
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
     </div>
 @endif
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::group(['middleware' => 'auth'], function() {
+
+	Route::get('/', function () {
+	    return view('welcome');
+	});
+
+});
 require __DIR__.'/auth.php';
